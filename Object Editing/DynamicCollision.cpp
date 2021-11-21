23f9e8195df5a6527a8abff37b6498230a88f69e
kellsnc <kellspam@outlook.fr>
2021-11-21 02:18:09 +0100

    update examples with symbol includes

--- Object Editing/DynamicCollision.cpp
+++ Object Editing/DynamicCollision.cpp
@@ -1,44 +1,54 @@
 #include "SADXModLoader.h"
 
-// Example of a simple custom object with dynamic collisions that does not move.
+// Example of a simple custom object with a static geometric collision.
 
-void __cdecl YourObjectRender(ObjectMaster* obj) {
-	if (!MissedFrames) { // don't render if the current frame is skipped
+void __cdecl YourObjectDisplay(task* tp)
+{
+	if (!MissedFrames) // Don't render if the current frame is skipped
+	{
 		njPushMatrixEx();
 		njSetTexture(TEXLIST);
-		njTranslateEx(&obj->Data1->Position);
-		njRotateEx((Angle*)&obj->Data1->Rotation, 0);
-		njDrawModel_SADX(MODEL);
+		njTranslateEx(&tp->twp->pos);
+		njRotateEx((Angle*)&tp->twp->ang, 0);
+		dsDrawModel(MODEL);
 		njPopMatrixEx();
 	}
 }
 
-void __cdecl YourObjectExec(ObjectMaster* obj) {
-	if (!ClipSetObject(obj)) { // This deletes the object if the player is too far away
-		obj->DisplaySub(obj); // Run the display
+void __cdecl YourObjectExec(task* tp)
+{
+	if (!CheckRangeOut(tp)) // This deletes the object if the player is too far away
+	{
+		tp->disp(tp); // Run the display
 	}
 }
 
-void __cdecl YourObjectDelete(ObjectMaster* obj) {
-	DynamicCOL_Remove(obj, obj->Data1->Object); // Release the dyncol if the object is deleted
+void __cdecl YourObjectDestroy(task* tp)
+{
+	auto landcol = (obj*)tp->twp->timer.ptr;
+
+	WithdrawCollisionEntry(tp, landcol);
+	ReleaseMobileLandObject(landcol);
 }
 
-void __cdecl YourObjectInit(ObjectMaster* obj) {
-	obj->DeleteSub = YourObjectDelete;
-	obj->MainSub = YourObjectExec;
-	obj->DisplaySub = YourObjectRender;
+void __cdecl YourObjectInit(task* tp)
+{
+	tp->dest = YourObjectDestroy;
+	tp->exec = YourObjectExec;
+	tp->disp = YourObjectDisplay;
 
-	NJS_OBJECT* object = ObjectArray_GetFreeObject();
+	auto twp = tp->twp;
+	auto object = GetMobileLandObject();
 
 	// The dyncol position
-	object->pos[0] = obj->Data1->Position.x;
-	object->pos[1] = obj->Data1->Position.y;
-	object->pos[2] = obj->Data1->Position.z;
+	object->pos[0] = twp->pos.x;
+	object->pos[1] = twp->pos.y;
+	object->pos[2] = twp->pos.z;
 
 	// The dyncol rotation
-	object->ang[0] = obj->Data1->Rotation.x;
-	object->ang[1] = obj->Data1->Rotation.y;
-	object->ang[2] = obj->Data1->Rotation.z;
+	object->ang[0] = twp->ang.x;
+	object->ang[1] = twp->ang.y;
+	object->ang[2] = twp->ang.z;
 
 	// The dyncol scale, we set it to normal (1.0f)
 	object->scl[0] = 1.0f;
@@ -48,8 +58,8 @@ void __cdecl YourObjectInit(ObjectMaster* obj) {
 	// The dyncol model
 	object->basicdxmodel = MODEL;
 
-	// Initialize (the dyncol will run until DynamicCol_Remove is called on the object)
-	DynamicCOL_Add(ColFlags_Solid, obj, object);
+	// Initialize with solid attribute
+	RegisterCollisionEntry(ColFlags_Solid, tp, object);
 
-	obj->Data1->Object = object; // Store the dyncol into our object to edit it or remove it later
+	twp->timer.ptr = object; // Store the dyncol into our object to remove it later
 }
--- Object Editing/MovingDynamicCollision.cpp
+++ Object Editing/MovingDynamicCollision.cpp
@@ -2,69 +2,81 @@
 
 static constexpr int MaxPlayers = 8;
 
-// Example of a custom object with dynamic collisions that moves.
+// Example of a custom object with a geometric collision that moves.
 
-void __cdecl YourObjectRender(ObjectMaster* obj) {
-	if (!MissedFrames) { // don't render if the current frame is skipped
+void __cdecl YourObjectDisplay(task* tp)
+{
+	if (!MissedFrames) // Don't render if the current frame is skipped
+	{
 		njPushMatrixEx();
 		njSetTexture(TEXLIST);
-		njTranslateEx(&obj->Data1->Position);
-		njRotateEx((Angle*)&obj->Data1->Rotation, 0);
-		njDrawModel_SADX(MODEL);
+		njTranslateEx(&tp->twp->pos);
+		njRotateEx((Angle*)&tp->twp->ang, 0);
+		dsDrawModel(MODEL);
 		njPopMatrixEx();
 	}
 }
 
-void __cdecl YourObjectExec(ObjectMaster* obj) {
-	if (!ClipSetObject(obj)) { // This deletes the object if the player is too far away
-		EntityData1* data = obj->Data1;
+void __cdecl YourObjectExec(task* tp)
+{
+	if (!CheckRangeOut(tp)) // This deletes the object if the player is too far away
+	{
+		auto twp = tp->twp;
+		auto landcol = (obj*)tp->twp->timer.ptr;
 
 		// Some very basic movement
-		data->Position.x += 0.01f;
-		data->Position.y += 0.1f;
-		data->Position.z += 0.05f;
+		twp->pos.x += 0.01f;
+		twp->pos.y += 0.1f;
+		twp->pos.z += 0.05f;
 
-		// Move players with the dyncol
-		for (int i = 0; i < MaxPlayers; ++i) {
-			CharObj2* co2 = CharObj2Ptrs[i];
-
-			if (co2 && co2->field_6C == obj) {
-				NJS_VECTOR offset = data->Position;
-				njSubVector(&offset, (NJS_VECTOR*)&data->Object->pos); // Get the difference between current position and previous position (where the dyncol currently is)
-				njAddVector(&EntityData1Ptrs[i]->Position, &offset); // Add the difference to the player position
+		// Loop for all player slots
+		for (int i = 0; i < MaxPlayers; ++i)
+		{
+			// Move the player if on the collision
+			if (CheckPlayerRideOnMobileLandObjectP(i, tp))
+			{
+				NJS_VECTOR offset = twp->pos;
+				njSubVector(&offset, (NJS_VECTOR*)&landcol->pos); // Get the difference between current position and previous position (where the dyncol currently is)
+				njAddVector(&playertwp[i]->pos, &offset); // Add the difference to the player position
 			}
 		}
 
 		// Update the dyncol position to the current position
-		data->Object->pos[0] = data->Position.x;
-		data->Object->pos[1] = data->Position.y;
-		data->Object->pos[2] = data->Position.z;
+		landcol->pos[0] = twp->pos.x;
+		landcol->pos[1] = twp->pos.y;
+		landcol->pos[2] = twp->pos.z;
 
 		// Run the display
-		obj->DisplaySub(obj);
+		tp->disp(tp);
 	}
 }
 
-void __cdecl YourObjectDelete(ObjectMaster* obj) {
-	DynamicCOL_Remove(obj, obj->Data1->Object); // Release the dyncol if the object is deleted
+void __cdecl YourObjectDestroy(task* tp)
+{
+	auto landcol = (obj*)tp->twp->timer.ptr;
+
+	WithdrawCollisionEntry(tp, landcol);
+	ReleaseMobileLandObject(landcol);
 }
 
-void __cdecl YourObjectInit(ObjectMaster* obj) {
-	obj->DeleteSub = YourObjectDelete;
-	obj->MainSub = YourObjectExec;
-	obj->DisplaySub = YourObjectRender;
+void __cdecl YourObjectInit(task* tp)
+{
+	tp->dest = YourObjectDestroy;
+	tp->exec = YourObjectExec;
+	tp->disp = YourObjectDisplay;
 
-	NJS_OBJECT* object = ObjectArray_GetFreeObject();
+	auto twp = tp->twp;
+	auto object = GetMobileLandObject();
 
 	// The dyncol position
-	object->pos[0] = obj->Data1->Position.x;
-	object->pos[1] = obj->Data1->Position.y;
-	object->pos[2] = obj->Data1->Position.z;
+	object->pos[0] = twp->pos.x;
+	object->pos[1] = twp->pos.y;
+	object->pos[2] = twp->pos.z;
 
 	// The dyncol rotation
-	object->ang[0] = obj->Data1->Rotation.x;
-	object->ang[1] = obj->Data1->Rotation.y;
-	object->ang[2] = obj->Data1->Rotation.z;
+	object->ang[0] = twp->ang.x;
+	object->ang[1] = twp->ang.y;
+	object->ang[2] = twp->ang.z;
 
 	// The dyncol scale, we set it to normal (1.0f)
 	object->scl[0] = 1.0f;
@@ -74,10 +86,8 @@ void __cdecl YourObjectInit(ObjectMaster* obj) {
 	// The dyncol model
 	object->basicdxmodel = MODEL;
 
-	// Initialize (the dyncol will run until DynamicCol_Remove is called on the object)
-	// Note: 0x08000000 is the dyncol for collisions that move
-	// If you don't set it, the dyncol will not update every frame
-	DynamicCOL_Add((ColFlags)(0x08000000 | ColFlags_Solid), obj, object);
+	// Initialize with dynamic attribute
+	RegisterCollisionEntry(ColFlags_Dynamic | ColFlags_Solid, tp, object);
 
-	obj->Data1->Object = object; // Store the dyncol into our object to edit it and remove it later
+	twp->timer.ptr = object; // Store the dyncol into our object to edit it and remove it later
 }
